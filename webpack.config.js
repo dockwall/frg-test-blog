a4f47e8f60826461ba553bc5eef9b0d24223beda
Stas <leshuksu@gmail.com>
2022-11-16 17:48:39 +0300

    Added cleanup and source maps to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js'
+        filename: '[name][contenthash].js',
+        clean: true
     },
     devServer: {
         static: {
