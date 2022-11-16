a1f21fb51814c286d7645227d89fc3f0557aeed8
Stas <leshuksu@gmail.com>
2022-11-16 17:44:46 +0300

    Configured webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,16 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js'
     },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true
+    },
     module: {
         rules: [
             {
